311f2e350bb0a34f1fb5dc346c99eabc4fd187d9
Joshua M. Schmidlkofer <joshua@joshuainnovates.us>
2022-11-29 03:34:40 +0000

    update: futzing with DHCP

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "jsinnovates"
 
     workspaces {
       name = "homelab-external"
--- platform/gitea/files/config/go.mod
+++ platform/gitea/files/config/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/gitea/config
+module git.ashbyte.com/khuedoan/homelab/gitea/config
 
 go 1.18
 
